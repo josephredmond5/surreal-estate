d009a998925c1d01338fdc1039000e200c305eb2
josephredmond5 <josephredmond5@icloud.com>
2023-04-12 21:15:30 +0100

    unordered list and list added with hyperlinks

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,8 +6,19 @@ const NavBar = () => {
         <div className="navbar">
             This is the NavBar component.
             <img className="logo" src='/images/surreal-logo.png' alt="Surreal Logo" />
+
+            <ul className="navbar-links">
+  <li className="navbar-links-item">
+    <a href="/properties">View Properties</a>
+  </li>
+  <li className="navbar-links-item">
+    <a href="/add-property">Add a Property</a>
+  </li>
+</ul>  
         </div>
     );
 };
 
 export default NavBar;
+
+
