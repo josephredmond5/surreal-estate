f331cb32386aa4bbe4348812f6edb158f95b55cb
josephredmond5 <josephredmond5@icloud.com>
2023-04-12 19:06:48 +0100

    pic added

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/navbar.css";
 
 const NavBar = () => {
     return(
         <div className="navbar">
             This is the NavBar component.
-             <img className="logo" src="/path/to/image/Surreal_Logo.png" alt="Surreal Logo" />
+            <img className="logo" src='/images/surreal-logo.png' alt="Surreal Logo" />
         </div>
     );
 };
