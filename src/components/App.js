d18e2deed0be8ca4c85c511e6ace176b58e8183c
josephredmond5 <josephredmond5@icloud.com>
2023-04-17 14:44:34 +0100

    add-property part of the module completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./navBar";
-import Properties from "./Properties";
-import AddProperty from "./AddProperty";
-import "../styles/navbar.css";
+import React from 'react';
+import NavBar from './navBar';
+import Properties from './Properties';
+import AddProperty from './AddProperty';
+import '../styles/navbar.css';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2 className="title-name">surreal estate</h2>
-      </header>
       <NavBar />
       <Routes>
         <Route path="/" element={<Properties />} />
@@ -21,3 +18,4 @@ const App = () => {
 };
 
 export default App;
+
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,15 +9,16 @@ const NavBar = () => {
             <img className="logo" src='/images/surreal-logo.png' alt="Surreal Logo" />
 
             <ul className="navbar-links">
-  <li className="navbar-links-item">
-    <Link href="/properties">View Properties</Link>
-  </li>
-  <li className="navbar-links-item">
-    <Link href="/add-property">Add a Property</Link>
-  </li>
-</ul>  
+              <li className="navbar-links-item">
+                <Link to="/">View Properties</Link>
+              </li>
+              <li className="navbar-links-item">
+                <Link to="/add-property">Add a Property</Link>
+              </li>
+            </ul>  
         </div>
     );
 };
 
 export default NavBar;
+
