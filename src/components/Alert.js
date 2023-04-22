0239f0bbfd575bc70eaff7a5491230e9dec3b393
Leah Burke <leahburke@leahs-air.home>
2023-04-22 21:45:33 +0100

    first two tests passed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Alert = ({ message }) => (
-  <div className="alert">
-    {message}
-  </div>
-);
+const Alert = ({ message, success }) => {
+  return (
+    <div className={`alert alert-${success ? "success" : "error"}`}>
+      {message}
+    </div>
+  );
+};
+
+console.log(Alert);
 
 export default Alert;
