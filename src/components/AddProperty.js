52fbd1748d27787f00f20a9dacd886ec9cb288a6
josephredmond5 <josephredmond5@icloud.com>
2023-04-24 22:09:48 +0100

    update to add property and alert files

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -44,90 +44,50 @@ const AddProperty = () => {
 
   return (
     <div className="add-property">
-      <h2>Add Property</h2>
-      <form onSubmit={handleAddProperty}>
-        {alert.message && (
-          <Alert message={alert.message} success={alert.isSuccess} />
-        )}
-        <label htmlFor="title">
-          Title
-          <input
-            id="title"
-            name="title"
-            type="text"
-            value={fields.title}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="type">
-          Type
-          <select
-            id="type"
-            name="type"
-            value={fields.type}
-            onChange={handleFieldChange}
-          >
-            <option value="Flat">Flat</option>
-            <option value="Detached">Detached</option>
-            <option value="Semi-Detached">Semi-Detached</option>
-            <option value="Terraced">Terraced</option>
-            <option value="End of Terrace">End of Terrace</option>
-            <option value="Cottage">Cottage</option>
-            <option value="Bungalow">Bungalow</option>
-          </select>
-        </label>
-        <label htmlFor="bedrooms">
-          Bedrooms
-          <input
-            id="bedrooms"
-            name="bedrooms"
-            type="number"
-            min="0"
-            value={fields.bedrooms}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="bathrooms">
-          Bathrooms
-          <input
-            id="bathrooms"
-            name="bathrooms"
-            type="number"
-            min="0"
-            value={fields.bathrooms}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="price">
-          Price (£)
-          <input
-            id="price"
-            name="price"
-            type="number"
-            min="0"
-            value={fields.price}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label htmlFor="city">
-          City
-          <select
-            id="city"
-            name="city"
-            value={fields.city}
-            onChange={handleFieldChange}
-          >
-            <option value="Manchester">Manchester</option>
-            <option value="Leeds">Leeds</option>
-            <option value="Sheffield">Sheffield</option>
-            <option value="Liverpool">Liverpool</option>
-          </select>
-        </label>
-        <button type="submit" value="submit">
-          Add
-        </button>
-      </form>
-    </div>
+  <h2>Add Property</h2>
+  <form onSubmit={handleAddProperty}>
+    <label htmlFor="title">
+      Title
+      <input
+        id="title"
+        name="title"
+        type="text"
+        value={fields.title}
+        onChange={handleFieldChange}
+      />
+    </label>
+    <label htmlFor="type">
+      Type
+      <select
+        id="type"
+        name="type"
+        value={fields.type}
+        onChange={handleFieldChange}
+      >
+        <option value="Flat">Flat</option>
+        <option value="Detached">Detached</option>
+        <option value="Semi-Detached">Semi-Detached</option>
+        <option value="Terraced">Terraced</option>
+        <option value="End of Terrace">End of Terrace</option>
+        <option value="Cottage">Cottage</option>
+        <option value="Bungalow">Bungalow</option>
+      </select>
+    </label>
+    <label htmlFor="bedrooms">
+      Bedrooms
+      <input
+        id="bedrooms"
+        name="bedrooms"
+        type="number"
+        min="0"
+        value={fields.bedrooms}
+        onChange={handleFieldChange}
+      />
+    </label>
+    <Alert message={alert.message} success={alert.isSuccess} />
+    <button type="submit">Add Property</button>
+  </form>
+</div>
   );
 };
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Alert = ({ message, success }) => {
+  if (!message) return null;
+
   return (
     <div className={`alert alert-${success ? "success" : "error"}`}>
       {message}
