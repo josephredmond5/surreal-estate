a496316a8de29f87a5287fabe785feba6d7940ab
josephredmond5 <josephredmond5@icloud.com>
2023-04-26 22:03:40 +0100

    final push before i start my final project

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -2,8 +2,22 @@ import React from "react";
 import App from "./App";
 import "../styles/property-card.css";
 
-const PropertyCard = () => {
+const PropertyCard = ({ 
+    title,
+    type,
+    bathrooms,
+    bedrooms,
+    price,
+    city,
+    email,
+  }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+  }
 
-}
+
 
 export default PropertyCard;
